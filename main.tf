0a8370af57316f5cde477a7465ae85749298c60c
windows 10 <dimpulsunayana205@gmail.com>
2023-02-22 17:46:12 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command =  "echo ${var.env}"
-  }
+module "tf-module-vpc" {
+  source = "https://github.com/Dimpulsunayana/tf-module-vpc.git"
+
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
 }
+
